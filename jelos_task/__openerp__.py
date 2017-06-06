2bcb3cf37ba068f0ddf04813736d333ac4f1afcc
Alvarotro7 <anavarro@bisnesmart.com>
2017-06-06 14:35:56 +0200

    modulo task cambio vista sin columna progress
    columna progress invisible

---
+++ jelos_task/__openerp__.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
+{
+    "name": "Jelos Task",
+    "version": "1.0",
+    "author": "BisneSmart Team,"
+              "BisneSmart",
+    "contributors": [
+        "Gonzalo Borras <gborras@bisnesmart.com>",
+        "Alvaro Navarro <anavarro@bisnesmart.com>",
+        "Departamento Odoo <odoo@bisnesmart.com>",
+    ],
+    "website": "http://www.bisnesmart.com",
+    "depends": [
+        'project',
+        'jelos_other',
+    ],
+    "data": [
+        "views/jelos_task.xml",
+    ],
+    "installable": True,
+    "application": True,
+
+}
